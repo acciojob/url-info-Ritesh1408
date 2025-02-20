a5ff3843bca1a8e19850025ddbf45cf374a0bd63
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-20 23:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+function getLength() {
+    let str = window.location.href; // Get the current page URL
+    return str.length; // Return the length of the URL
+}
+
+alert(`The length of the URL is: ${getLength()}`);
